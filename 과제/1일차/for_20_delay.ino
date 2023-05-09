afa6c29bcfcd082b5b30b82584590e6168c1d26e
Blue_bla <73012354+Bluebla1004@users.noreply.github.com>
2023-05-09 11:15:08 +0900

    Update for_20_delay.ino

--- 과제/1일차/for_20_delay.ino
+++ 과제/1일차/for_20_delay.ino
@@ -2,18 +2,18 @@ int Pin13 = 13;
 int delayPeriod = 200;
 int num = 0;
 
-void setup() 
+void setup()
 {
     pinMode(Pin13, OUTPUT);
 }
 
-void loop() 
+void loop()
 {
    for(num; num<20; num++)
        {
-        digitalWrite(Pin13, HIGH);   
-        delay(delayPeriod);              
-        digitalWrite(Pin13, LOW); 
+        digitalWrite(Pin13, HIGH);
+        delay(delayPeriod);
+        digitalWrite(Pin13, LOW);
         delay(delayPeriod);
        }
      delay(3000);
