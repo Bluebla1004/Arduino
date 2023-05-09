d881a9a819b967c0c3df10724b8d5e4ff753a8c7
Blue_bla <73012354+Bluebla1004@users.noreply.github.com>
2023-05-09 10:54:15 +0900

    Create if 사용해보기.ino

---
+++ 과제/1일차/if 사용해보기.ino
@@ -0,0 +1,23 @@
+int Pin13 = 13;
+int delayPeriod = 500;
+
+void setup() 
+{
+    pinMode(Pin13, OUTPUT);
+}
+
+void loop() 
+{
+    digitalWrite(Pin13, HIGH);   
+    delay(delayPeriod);         
+
+    digitalWrite(Pin13, LOW); 
+    delay(delayPeriod);
+
+    delayPeriod = delayPeriod + 100;
+
+    if(delayPeriod >= 3000)
+      {
+           delayPeriod = 500;
+      }
+}
